75daf366a3cfeb53f0719227a7f96918bbe09a83
CodeWithRamkumar <ramkumar03251999@gmail.com>
2025-06-28 10:15:21 +0530

    First commit of node js music app

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
